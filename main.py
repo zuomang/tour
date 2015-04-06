ce1b78a4b5b36d2cf7a6c20800f8ddc877eb6bb3
zuomang <zuomangjk@gmail.com>
2015-04-06 21:00:25 +0800

    add info handle

--- main.py
+++ main.py
@@ -3,21 +3,31 @@ import time, hashlib
 
 app = Flask(__name__)
 
-@app.route('/weixin', methods=['GET'])
+app.config.update(
+        SERVER_NAME='localhost',
+        DEBUG=True
+        )
+@app.route('/', methods=['GET'])
 def wechat_auth():
-	if request.method == 'GET':
-		token = "genchlife"
-		query = request.args
-		signature = query.get('signature', '')
-		timestamp = query.get('timestamp', '')
-		nonce = query.get('nonce', '')
-		echostr = query.get('echostr', '')
+    if request.method == 'GET':
+        token = "test"
+        query = request.args
+        signature = query.get('signature', '')
+        timestamp = query.get('timestamp', '')
+        nonce = query.get('nonce', '')
+        echostr = query.get('echostr', '')
 
-		s = [timestamp, nonce, token]
-		s.sort()
-		s = ''.join(s)
-		if (hashlib.sha1(s).hexdigest() == signature):
-			return make_response(echostr)
+        s = [timestamp, nonce, token]
+        s.sort()
+        s = ''.join(s)
+        if (hashlib.sha1(s).hexdigest() == signature):
+            return make_response(echostr)
+
+@app.route('/info', methods=['GET'])
+def info():
+    if request.method == 'GET':
+        print request.url
+        return 'hello, world'
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	app.run(port=80)
