74fc4a4beca589430b28d80f4db6d80bde6f2273
zuomang <zuomangjk@gmail.com>
2015-04-11 17:56:45 +0800

    update

--- util.py
+++ util.py
@@ -3,32 +3,14 @@
 
 from datetime import datetime
 import requests
-from flask import session
+from flask import session, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
-from models.User import User
+from models.user import User
+import parameter
 
 def now():
     return datetime.now()
 
-# @app.before_request
-# def before_request(*args, **keys):
-#     if request.method == 'GET':
-#         code = request.args.get('code', '')
-#         data = {
-#             'appid': parameter.appid,
-#             'secret': parameter.appsecret,
-#             'code': code,
-#             'grant_type': 'authorization_code'
-#         }
-#         result = requests.get('https://api.weixin.qq.com/sns/oauth2/access_token', params = data)
-#         id = result.json().get('openid', '')
-#         r = Token.query.filter_by(openid = id).first()
-#         if r:
-#             print 'user had login'
-#             return 'success'
-#         else:
-#             print 'user not login'
-#             return 'error'
 def check_bing(request):
  	code = request.args.get('code', '')
 	data = {
