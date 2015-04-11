55052c7d43fc67e0d8addd5c54e188eabaae1a9b
zuomang <zuomangjk@gmail.com>
2015-04-11 17:06:44 +0800

    update

--- models/User.py
+++ models/User.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:qwaszx12@localhost/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:zmyjy1314@localhost/test'
 db = SQLAlchemy(app)
 
 
--- models/token.py
+++ models/token.py
@@ -6,7 +6,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:qwaszx12@localhost/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:zmyjy1314@localhost/test'
 db = SQLAlchemy(app)
 
 
