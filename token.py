848d5f3f852d3dbddf498364e99b61ecb5f90bc6
zuomang <zuomangjk@gmail.com>
2015-04-11 17:30:47 +0800

    update

--- token.py
+++ token.py
@@ -1,22 +1,21 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:zmyjy1314@localhost/test'
 db = SQLAlchemy(app)
 
-class Token(db.Model):
-    token = db.Column(db.String(20), primary_key=True)
-    value = db.Column(db.String(80))
-    date = db.Column(db.DateTime)
 
-    def __init__(self, token, value, datetime):
-        self.token = token
+class Token(db.Model):
+    env = db.Column(db.String(20), primary_key=True)
+    value = db.Column(db.String(80))
+    datetime = db.Column(db.DateTime)
+
+    def __init__(self, env, value, datetime):
+        self.env = env
         self.value = value
-        self.datetime = datetime
+        sele.datetime = datetime
 
     def __repr__(self):
-        return '<Token %r>' % self.token
+        return '<env %r>' % self.env
