a18e8e9d6b5014c9f06e5ca280df8fa50fb345dd
zuomang <zuomangjk@gmail.com>
2015-04-11 17:52:45 +0800

    update

---
+++ models/user.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:zmyjy1314@localhost/test'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    openid = db.Column(db.String(80), primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    phone = db.Column(db.String(11), unique=True)
+
+    def __init__(self, openid, username, phone):
+        self.openid = openid
+        self.username = username
+        self.phone = phone
+
+    def __repr__(self):
+        return '<User %r>' % self.username
