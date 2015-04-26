681b9e982ebf7cbb909ab8c349a68991e71962d0
zuomang <zuomangjk@gmail.com>
2015-04-26 16:39:13 +0800

    update创建群

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 from flask import Flask, request, make_response, render_template, g, session
 from flask.ext.sqlalchemy import SQLAlchemy
 from models.table import Custormer, Qun, member
