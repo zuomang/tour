94df55ff765f129fc87f1cf1139f109805ab6d90
zuomang <zuomangjk@gmail.com>
2015-04-27 21:22:33 +0800

    update

--- util.py
+++ util.py
@@ -9,17 +9,18 @@ from models.table import Custormer
 import parameter
 
 def now():
-    return datetime.now()
+	return datetime.now()
 
 def check_bing(request):
- 	code = request.args.get('code', '')
+	code = request.args.get('code', '')
 	data = {
-    	'appid': parameter.appid,
-    	'secret': parameter.appsecret,
-    	'code': code,
-    	'grant_type': 'authorization_code'
+		'appid': parameter.appid,
+		'secret': parameter.appsecret,
+		'code': code,
+		'grant_type': 'authorization_code'
 	}
 	result = requests.get('https://api.weixin.qq.com/sns/oauth2/access_token', params = data)
 	id = result.json().get('openid')
 	session['openid'] = id
-	return Custormer.query.filter_by(openid = id).first()
+	user =  Custormer.query.filter_by(openid = id).first()
+	return user
