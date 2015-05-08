e12bfd204a419e4440d6daa2f063d0435837dcfe
zuomang <zuomangjk@gmail.com>
2015-05-08 16:34:50 +0800

    update

--- models/table.py
+++ models/table.py
@@ -38,9 +38,9 @@ class Qun(db.Model):
 	register_time = db.Column(db.DateTime, default = datetime.now())
 	rank = db.Column(db.Integer, nullable = False, default = 1)
 	member_count = db.Column(db.Integer, default = 1)
-	building_fund = db.Column(db.Integer, default = 0)
-	extracted_fund = db.Column(db.Integer, default = 0)
-	balance_fund = db.Column(db.Integer, default = 0)
+	building_fund = db.Column(db.Float, default = 0)
+	extracted_fund = db.Column(db.Float, default = 0)
+	balance_fund = db.Column(db.Float, default = 0)
 	openid = db.Column(db.String(40), db.ForeignKey('custormer.openid'))
 
 	def __init__(self, name, phone, openid, building_fund):
@@ -69,8 +69,8 @@ class Activity(db.Model):
 	name = db.Column(db.String(20), nullable = False)
 	owner = db.Column(db.String(28), nullable = False)
 	description = db.Column(db.String(200))
-	partici_fee = db.Column(db.String(20), nullable = False)
-	cost = db.Column(db.String(20), nullable = False)
+	partici_fee = db.Column(db.Float, nullable = False)
+	cost = db.Column(db.Float, nullable = False)
 
 	def __init__(self, name, owner, partici_fee, cost):
 		self.name = name
@@ -81,5 +81,30 @@ class Activity(db.Model):
 	def __repr__(self):
 		return '<Activity %r>' % self.name
 
+class ActivityDetail(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	activity_id = db.Column(db.Integer, db.ForeignKey('activity.id'))
+	activity_name = db.Column(db.String(20), nullable = False)
+	activity_date = db.Column(db.String(20), nullable = False)
+	qunownr_id = db.Column(db.String(28), db.ForeignKey('custormer.openid'))
+	custormer_id = db.Column(db.String(28), db.ForeignKey('custormer.openid'))
+	accompany_count = db.Column(db.Integer, nullable = False)
+	activity_pay = db.Column(db.Float, nullable = False)
+	qunbuilding_return = db.Column(db.Float, nullable = False)
+
+	def __init__(self, activity_id, activity_name, activity_date, qunownr_id,
+		custormer_id, accompany_count, activity_pay, qunbuilding_return):
+		self.activity_id = activity_id
+		self.activity_name = activity_name
+		self.activity_date = activity_date
+		self.qunownr_id = qunownr_id
+		self.custormer_id = custormer_id,
+		self.accompany_count = accompany_count
+		self.activity_pay = activity_pay
+		self.qunbuilding_return = qunbuilding_return
+
+	def __repr__(self):
+		return '<ActivityDetail %r>' %self.id
+		
 if __name__ == '__main__':
 	db.create_all()
